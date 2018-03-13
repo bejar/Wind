c33ca7775bd3a592554b95085407a04116d48841
javier.bejar <bejar@cs.upc.edu>
2018-03-13 13:54:51 +0100

    Batch experiments and then some

--- Experiments/WindPredictionRNNBatch.py
+++ Experiments/WindPredictionRNNBatch.py
@@ -37,6 +37,8 @@ from time import time
 from Wind.Util import load_config_file
 from Wind.Data import generate_dataset
 from Wind.Config import wind_data_path
+import itertools
+from copy import deepcopy
 
 __author__ = 'bejar'
 
@@ -133,10 +135,33 @@ def architecture(neurons, drop, nlayers, activation, activation_r, rnntype, CuDN
 
     return model
 
+def generate_configs(config):
+    """
+    Generates all possible individual configs from the fields with multiple values
+    :param config:
+    :return:
+    """
+    lconf = [{}]
+
+    for f1 in config:
+        for f2 in config[f1]:
+            lnconf = []
+            for v in config[f1][f2]:
+                for c in lconf:
+                    cp = deepcopy(c)
+                    if f1 in cp:
+                        cp[f1][f2] = v
+                    else:
+                        cp[f1] = {f2: v}
+                    lnconf.append(cp)
+            lconf = lnconf
+
+    return lconf
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default='config2', help='Experiment configuration')
+    parser.add_argument('--config', default='configBatch1', help='Experiment configuration')
     parser.add_argument('--verbose', help="Verbose output (enables Keras verbose output)", action='store_true',
                         default=False)
     parser.add_argument('--gpu', help="Use LSTM/GRU gpu implementation", action='store_true', default=False)
@@ -147,108 +172,137 @@ if __name__ == '__main__':
     verbose = 1 if args.verbose else 0
     impl = 2 if args.gpu else 1
 
-    config = load_config_file(args.config)
+    configB = load_config_file(args.config)
+
+    for dname in configB['data']['datanames']:
+        resfile = open('result-%s.txt'%dname[0], 'a')
+        resfile.write('DNAME, DATAS, VARS, LAG, AHEAD, RNN, Bi, NLAY, NNEUR, DROP, ACT, RACT, '
+                      'OPT, R2Val, R2persV, R2Test, R2persT')
+        resfile.close()
+
     ############################################
     # Data
 
-    sahead = config['data']['ahead']
+    for config in generate_configs(configB):
 
-    for ahead in range(1, sahead + 1):
+        sahead = config['data']['ahead']
 
-        if args.verbose:
-            print('-----------------------------------------------------------------------------')
-            print('Steps Ahead = %d ' % ahead)
+        for ahead in range(1, sahead + 1):
 
-        train_x, train_y, val_x, val_y, test_x, test_y = generate_dataset(config['data'], ahead=ahead, mode=False,
-                                                                          data_path=wind_data_path)
+            if args.verbose:
+                print('-----------------------------------------------------------------------------')
+                print('Steps Ahead = %d ' % ahead)
 
-        ############################################
-        # Model
+            train_x, train_y, val_x, val_y, test_x, test_y = generate_dataset(config['data'], ahead=ahead, mode=False,
+                                                                              data_path=wind_data_path)
 
-        neurons = config['arch']['neurons']
-        drop = config['arch']['drop']
-        nlayers = config['arch']['nlayers']  # >= 1
+            ############################################
+            # Model
 
-        activation = config['arch']['activation']
-        activation_r = config['arch']['activation_r']
-        rec_reg = config['arch']['rec_reg']
-        rec_regw = config['arch']['rec_regw']
-        k_reg = config['arch']['k_reg']
-        k_regw = config['arch']['k_regw']
-        bidirectional = config['arch']['bidirectional']
-        bimerge = config['arch']['bimerge']
+            neurons = config['arch']['neurons']
+            drop = config['arch']['drop']
+            nlayers = config['arch']['nlayers']  # >= 1
 
-        model = architecture(neurons=neurons, drop=drop, nlayers=nlayers, activation=activation,
-                             activation_r=activation_r, rnntype=config['arch']['rnn'], CuDNN=config['arch']['CuDNN'],
-                             rec_reg=rec_reg, rec_regw=rec_regw, k_reg=k_reg, k_regw=k_regw,
-                             bidirectional=bidirectional, bimerge=bimerge,
-                             full=config['arch']['full'])
-        if args.verbose:
-            model.summary()
+            activation = config['arch']['activation']
+            activation_r = config['arch']['activation_r']
+            rec_reg = config['arch']['rec_reg']
+            rec_regw = config['arch']['rec_regw']
+            k_reg = config['arch']['k_reg']
+            k_regw = config['arch']['k_regw']
+            bidirectional = config['arch']['bidirectional']
+            bimerge = config['arch']['bimerge']
 
-            print('lag: ', config['data']['lag'], '/Neurons: ', neurons, '/Layers: ', nlayers, '/Activation:',
-                  activation, activation_r)
-            print('Tr:', train_x.shape, train_y.shape, 'Val:', val_x.shape, val_y.shape, 'Ts:', test_x.shape,
-                  test_y.shape)
+            model = architecture(neurons=neurons, drop=drop, nlayers=nlayers, activation=activation,
+                                 activation_r=activation_r, rnntype=config['arch']['rnn'], CuDNN=config['arch']['CuDNN'],
+                                 rec_reg=rec_reg, rec_regw=rec_regw, k_reg=k_reg, k_regw=k_regw,
+                                 bidirectional=bidirectional, bimerge=bimerge,
+                                 full=config['arch']['full'])
+            if args.verbose:
+                model.summary()
+
+                print('lag: ', config['data']['lag'], '/Neurons: ', neurons, '/Layers: ', nlayers, '/Activation:',
+                      activation, activation_r)
+                print('Tr:', train_x.shape, train_y.shape, 'Val:', val_x.shape, val_y.shape, 'Ts:', test_x.shape,
+                      test_y.shape)
+                print()
+
+            ############################################
+            # Training
+            cbacks = []
+            if args.tboard:
+                tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+                cbacks.append(tensorboard)
+
+            if args.best:
+                modfile = './model%d.h5' % int(time())
+                mcheck = ModelCheckpoint(filepath=modfile, monitor='val_loss', verbose=0, save_best_only=True,
+                                         save_weights_only=False, mode='auto', period=1)
+                cbacks.append(mcheck)
+
+            optimizer = config['training']['optimizer']
+            if optimizer == 'rmsprop':
+                if 'lrate' in config['training']:
+                    optimizer = RMSprop(lr=config['training']['lrate'])
+                else:
+                    optimizer = RMSprop(lr=0.001)
+
+            model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+            batch_size = config['training']['batch']
+            nepochs = config['training']['epochs']
+
+            model.fit(train_x, train_y, batch_size=batch_size, epochs=nepochs, validation_data=(val_x, val_y),
+                      verbose=verbose, callbacks=cbacks)
+
+            ############################################
+            # Results
+
+            if args.best:
+                model = load_model(modfile)
+
+            score = model.evaluate(val_x, val_y, batch_size=batch_size, verbose=0)
+            print('MSE val= ', score)
+            print('MSE val persistence =', mean_squared_error(val_y[ahead:], val_y[0:-ahead]))
+            val_yp = model.predict(val_x, batch_size=batch_size, verbose=0)
+            r2val = r2_score(val_y, val_yp)
+            r2persV = r2_score(val_y[ahead:], val_y[0:-ahead])
+            print('R2 val= ', r2val)
+            print('R2 val persistence =', r2persV)
+
+            score = model.evaluate(test_x, test_y, batch_size=batch_size, verbose=0)
             print()
+            print('MSE test= ', score)
+            print('MSE test persistence =', mean_squared_error(test_y[ahead:], test_y[0:-ahead]))
+            test_yp = model.predict(test_x, batch_size=batch_size, verbose=0)
+            r2test = r2_score(test_y, test_yp)
+            r2persT = r2_score(test_y[ahead:, 0], test_y[0:-ahead, 0])
+            print('R2 test= ', r2test)
+            print('R2 test persistence =', r2persT)
 
-        ############################################
-        # Training
-        cbacks = []
-        if args.tboard:
-            tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
-            cbacks.append(tensorboard)
-
-        if args.best:
-            modfile = './model%d.h5' % int(time())
-            mcheck = ModelCheckpoint(filepath=modfile, monitor='val_loss', verbose=0, save_best_only=True,
-                                     save_weights_only=False, mode='auto', period=1)
-            cbacks.append(mcheck)
-
-        optimizer = config['training']['optimizer']
-        if optimizer == 'rmsprop':
-            if 'lrate' in config['training']:
-                optimizer = RMSprop(lr=config['training']['lrate'])
-            else:
-                optimizer = RMSprop(lr=0.001)
-
-        model.compile(loss='mean_squared_error', optimizer=optimizer)
-
-        batch_size = config['training']['batch']
-        nepochs = config['training']['epochs']
-
-        model.fit(train_x, train_y, batch_size=batch_size, epochs=nepochs, validation_data=(val_x, val_y),
-                  verbose=verbose, callbacks=cbacks)
-
-        ############################################
-        # Results
-
-        if args.best:
-            model = load_model(modfile)
-
-        score = model.evaluate(val_x, val_y, batch_size=batch_size, verbose=0)
-        print('MSE val= ', score)
-        print('MSE val persistence =', mean_squared_error(val_y[ahead:], val_y[0:-ahead]))
-        val_yp = model.predict(val_x, batch_size=batch_size, verbose=0)
-        r2val = r2_score(val_y, val_yp)
-        r2persV = r2_score(val_y[ahead:], val_y[0:-ahead])
-        print('R2 val= ', r2val)
-        print('R2 val persistence =', r2persV)
-
-        score = model.evaluate(test_x, test_y, batch_size=batch_size, verbose=0)
-        print()
-        print('MSE test= ', score)
-        print('MSE test persistence =', mean_squared_error(test_y[ahead:], test_y[0:-ahead]))
-        test_yp = model.predict(test_x, batch_size=batch_size, verbose=0)
-        r2test = r2_score(test_y, test_yp)
-        r2persT = r2_score(test_y[ahead:, 0], test_y[0:-ahead, 0])
-        print('R2 test= ', r2test)
-        print('R2 test persistence =', r2persT)
-
-        resfile = open('result.txt', 'a')
-        resfile.write('DATAS= %d, LAG= %d, AHEAD= %d, RNN= %s, Bi=%s, NLAY= %d, NNEUR= %d, DROP= %3.2f, ACT= %s, RACT= %s, '
+            resfile = open('result-%s.txt'%config['data']['datanames'][0], 'a')
+            resfile.write('%s, %d, %d, %d, %d, %s, %s, %d, %d, %3.2f, %s, %s, '
+                          '%s, %3.5f, %3.5f, %3.5f, %3.5f\n' %
+                          (config['data']['datanames'][0],
+                           config['data']['dataset'],
+                           len(config['data']['vars']),
+                           config['data']['lag'],
+                           config['data']['ahead'],
+                           config['arch']['rnn'],
+                           config['arch']['bimerge'] if config['arch']['bidirectional'] else 'no',
+                           config['arch']['nlayers'],
+                           config['arch']['neurons'],
+                           config['arch']['drop'],
+                           config['arch']['activation'],
+                           config['arch']['activation_r'],
+                           config['training']['optimizer'],
+                           r2val, r2persV, r2test, r2persT
+                           ))
+            resfile.close()
+            print('DNAME= %s, DATAS= %d, VARS= %d, LAG= %d, AHEAD= %d, RNN= %s, Bi=%s, NLAY= %d, NNEUR= %d, DROP= %3.2f, ACT= %s, RACT= %s, '
                       'OPT= %s, R2Val = %3.5f, R2persV = %3.5f, R2Test = %3.5f, R2persT = %3.5f\n' %
-                      (config['data']['dataset'],
+                      (config['data']['datanames'][0],
+                       config['data']['dataset'],
+                       len(config['data']['vars']),
                        config['data']['lag'],
                        config['data']['ahead'],
                        config['arch']['rnn'],
@@ -260,13 +314,12 @@ if __name__ == '__main__':
                        config['arch']['activation_r'],
                        config['training']['optimizer'],
                        r2val, r2persV, r2test, r2persT
-                       ))
-        resfile.close()
+                           ))
 
-        try:
-            os.remove(modfile)
-        except OSError:
-            pass
+            try:
+                os.remove(modfile)
+            except OSError:
+                pass
     # ----------------------------------------------
     # plt.subplot(2, 1, 1)
     # plt.plot(test_predict, color='r')
