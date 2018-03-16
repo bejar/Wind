9499e4a7eb666ced8d6e644eb473c70cdb907295
javier.bejar <bejar@cs.upc.edu>
2018-03-16 14:20:10 +0100

    Adding a script that gets configurations from a DB and more stuff

--- Scripts/ExpStatus.py
+++ Scripts/ExpStatus.py
@@ -30,9 +30,24 @@ if __name__ == '__main__':
     col = db[mongoconnection.col]
 
     exp = col.find({'status': 'done'})
-    print("Done = %d" % len([v for v in exp]))
+    jobs = [v for v in exp]
+    print("Done = %d" % len(jobs))
+    for i, j in enumerate(jobs):
+        if 'etime' in j:
+            print('JOB %d = %s'(i, j['etime']))
+        else:
+            print('JOB %d = ???' % i)
+
     exp = col.find({'status': 'working'})
-    print("Working = %d" % len([v for v in exp]))
+    jobs = [v for v in exp]
+    print("Working = %d" % len(jobs))
+
+    for i, j in enumerate(jobs):
+        if 'btime' in j:
+            print('JOB %d = %s'(i, j['btime']))
+        else:
+            print('JOB %d = ???' % i)
+
     exp = col.find({'status': 'pending'})
     print("Pending = %d" % len([v for v in exp]))
 
