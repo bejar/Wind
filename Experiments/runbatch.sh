7f4881e4710d9beb46c5f381678e50717443a358
javier.bejar <bejar@cs.upc.edu>
2018-03-16 14:25:30 +0100

    Bash script to run batch experiments

---
+++ Experiments/runbatch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+ COUNTER=0
+ while [  $COUNTER -lt 10 ]; do
+     python WindPredictionRNNBatchDB.py --best --nbatches 3
+     echo Run number $COUNTER
+     let COUNTER=COUNTER+1
+ done
