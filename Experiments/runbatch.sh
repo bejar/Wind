0cbfecc93319c91773e9093b751d425e7c6b7906
javier.bejar <bejar@cs.upc.edu>
2018-03-18 19:28:03 +0100

    monitoring code

--- Experiments/runbatch.sh
+++ Experiments/runbatch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 COUNTER=0
-while [  $COUNTER -lt 10 ]; do
-     python WindPredictionRNNBatchDB.py --best --nbatches 3
+while [  $COUNTER -lt 30 ]; do
+     python WindPredictionRNNBatchDB.py --best --nbatches 1
      echo Run number $COUNTER
      let COUNTER=COUNTER+1
 done
---
+++ Wind/Monitor/__init__.py
@@ -0,0 +1,19 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+******
+
+:Description: __init__.py
+
+    Different Auxiliary functions used for different purposes
+
+:Authors:
+    bejar
+
+:Version: 
+
+:Date:  18/03/2018
+"""
+
+__author__ = 'bejar'
