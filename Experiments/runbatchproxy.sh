a5cee64c7683e99bc1d39b44f239411fac485d5b
javier.bejar <bejar@cs.upc.edu>
2018-04-05 15:16:05 +0200

    Adding training with local configuration option

--- Experiments/runbatchproxy.sh
+++ Experiments/runbatchproxy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 COUNTER=0
 while [  $COUNTER -lt 30 ]; do
-     python WindPredictionRNNBatchDB.py --best --proxy --gpu
+     python WindPredictionRNNBatchDB.py --best --proxy --early --gpu
      echo Run number $COUNTER
      let COUNTER=COUNTER+1
 done
