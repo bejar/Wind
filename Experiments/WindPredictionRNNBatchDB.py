94fab4f0e49de081e8ccc237b620fe6f671d772f
javier.bejar <bejar@cs.upc.edu>
2018-04-05 08:59:28 +0200

    Adding mlp regression to training

--- Experiments/WindPredictionRNNBatchDB.py
+++ Experiments/WindPredictionRNNBatchDB.py
@@ -19,7 +19,7 @@ from __future__ import print_function
 import numpy as np
 
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM, GRU, CuDNNGRU, CuDNNLSTM, Bidirectional, TimeDistributed, Flatten, RepeatVector
 from keras.optimizers import RMSprop, SGD
 from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
@@ -232,6 +232,24 @@ def architectureS2S(ahead, idimensions, neurons, drop, nlayersE, nlayersD, activ
     return model
 
 
+def architectureMLP(idimensions, odimension, activation='linear', rec_reg='l1', rec_regw=0.1, k_reg='l1', k_regw=0.1,
+                    dropout=0.0, full_layers=[128]):
+    """
+    Arquitecture with direct regression using MLP
+
+    :return:
+    """
+    model = Sequential()
+    model.add(Dense(full_layers[0], input_shape=idimensions))
+    model.add(Dropout(rate=dropout))
+    for units in full_layers[1:]:
+        model.add(Dense(units=units, activation=activation))
+        model.add(Dropout(rate=dropout))
+
+    model.add(Dense(odimension))
+
+    return model
+
 
 def getconfig(proxy=False):
     """
@@ -429,7 +447,7 @@ def train_regdir_architecture(config, impl, verbose):
 
 def train_seq2seq_architecture(config, impl, verbose):
     """
-    Training process for architecture with direct regression of ahead time steps
+    Training process for RNN architecture with sequence to sequence regression of ahead time steps
 
     :return:
     """
@@ -493,7 +511,7 @@ def train_seq2seq_architecture(config, impl, verbose):
     nepochs = config['training']['epochs']
 
     model.fit(train_x, train_y, batch_size=batch_size, epochs=nepochs, validation_data=(val_x, val_y),
-              verbose=verbose, callbacks=[mcheck])
+              verbose=verbose, callbacks=cbacks)
 
     ############################################
     # Results
@@ -519,6 +537,108 @@ def train_seq2seq_architecture(config, impl, verbose):
 
 
     return lresults
+
+
+def train_MLP_regdir_architecture(config, verbose):
+    """
+     Training process for MLP architecture with regression of ahead time steps
+
+    :param config:
+    :param impl:
+    :param verbose:
+    :return:
+    """
+    ahead = config['data']['ahead']
+
+    train_x, train_y, val_x, val_y, test_x, test_y = generate_dataset(config['data'], ahead=ahead, mode='mlp',
+                                                                      data_path=wind_data_path)
+    print('Tr:', train_x.shape, train_y.shape, 'Val:', val_x.shape, val_y.shape, 'Ts:', test_x.shape, test_y.shape)
+
+    ############################################
+    # Model
+
+    neurons = config['arch']['neurons']
+    drop = config['arch']['drop']
+    nlayers = config['arch']['nlayers']  # >= 1
+
+    activation = config['arch']['activation']
+
+    rec_reg = config['arch']['rec_reg']
+    rec_regw = config['arch']['rec_regw']
+    k_reg = config['arch']['k_reg']
+    k_regw = config['arch']['k_regw']
+
+    dropout = config['arch']['drop']
+
+    model = architectureMLP(idimensions=train_x.shape[1:], odimension=config['data']['ahead'], activation=activation,
+                            rec_reg=rec_reg, rec_regw=rec_regw, k_reg=k_reg, k_regw=k_regw, dropout=dropout,
+                            full_layers=config['arch']['full'])
+
+    if args.verbose:
+
+        model.summary()
+
+        print('lag: ', config['data']['lag'], '/Neurons: ', neurons, '/Layers: ', nlayers, '/Activation:', activation)
+        print('Tr:', train_x.shape, train_y.shape, 'Val:', val_x.shape, val_y.shape, 'Ts:', test_x.shape, test_y.shape)
+        print()
+
+    ############################################
+    # Training
+    cbacks = []
+    if args.tboard:
+        tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+        cbacks.append(tensorboard)
+
+    if args.best:
+        modfile = './model%d.h5' % int(time())
+        mcheck = ModelCheckpoint(filepath=modfile, monitor='val_loss', verbose=0, save_best_only=True,
+                                 save_weights_only=False, mode='auto', period=1)
+        cbacks.append(mcheck)
+
+    if args.early:
+        early = EarlyStopping(monitor='val_loss', patience=10, verbose=0)
+        cbacks.append(early)
+
+    optimizer = config['training']['optimizer']
+    if optimizer == 'rmsprop':
+        if 'lrate' in config['training']:
+            optimizer = RMSprop(lr=config['training']['lrate'])
+        else:
+            optimizer = RMSprop(lr=0.001)
+
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+    batch_size = config['training']['batch']
+    nepochs = config['training']['epochs']
+
+    model.fit(train_x, train_y, batch_size=batch_size, epochs=nepochs, validation_data=(val_x, val_y),
+              verbose=verbose, callbacks=cbacks)
+
+    ############################################
+    # Results
+    if args.best:
+        model = load_model(modfile)
+
+    val_yp = model.predict(val_x, batch_size=batch_size, verbose=0)
+    test_yp = model.predict(test_x, batch_size=batch_size, verbose=0)
+
+    lresults = []
+
+    for i in range(1, ahead + 1):
+        lresults.append((i,
+                         r2_score(val_y[:, i - 1], val_yp[:, i - 1]),
+                         r2_score(val_y[i:, 0], val_y[0:-i, 0]),
+                         r2_score(test_y[:, i - 1], test_yp[:, i - 1]),
+                         r2_score(test_y[i:, 0], test_y[0:-i, 0])))
+
+    try:
+        os.remove(modfile)
+    except OSError:
+        pass
+
+
+    return lresults
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -548,5 +668,7 @@ if __name__ == '__main__':
             lresults = train_regdir_architecture(config, impl, verbose)
         elif config['arch']['mode'] == 'seq2seq':
             lresults = train_seq2seq_architecture(config, impl, verbose)
+        elif config['arch']['mode'] == 'mlp':
+            lresults = train_MLP_regdir_architecture(config, verbose)
 
         saveconfig(config, lresults, proxy=args.proxy)
