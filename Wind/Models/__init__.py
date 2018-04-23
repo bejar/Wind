07e26040e83f0fadcf6e8cb9e3d655d30fbcdf81
javier.bejar <bejar@cs.upc.edu>
2018-04-23 15:27:52 +0200

    Adding convolutional regression to batch training

--- Wind/Models/__init__.py
+++ Wind/Models/__init__.py
@@ -21,10 +21,11 @@ from .DirectRegression import architectureDirRegression, train_dirregression_arc
 from .MLPRegression import architectureMLP, train_MLP_regdir_architecture
 from .Seq2SeqRegression import architectureS2S, train_seq2seq_architecture
 from .Ensemble import train_ensemble_architecture
-
+from .ConvoRegression import train_convdirregression_architecture, architectureConvDirRegression
 __author__ = 'bejar'
 
 __all__ = ['architectureDirRegression', 'train_dirregression_architecture',
            'architectureMLP', 'train_MLP_regdir_architecture',
            'architectureS2S', 'train_seq2seq_architecture',
-           'train_ensemble_architecture']
+           'train_ensemble_architecture',
+           'train_convdirregression_architecture', 'architectureConvDirRegression']
