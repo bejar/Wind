ae17fa9cada813f3fe90f686baee5761591d3ffa
javier.bejar <bejar@cs.upc.edu>
2018-04-24 13:45:23 +0200

    Adding convolutional regression to batch training

--- Experiments/runbatch.sh
+++ Experiments/runbatch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 COUNTER=0
-while [  $COUNTER -lt 150 ]; do
+while [  $COUNTER -lt 250 ]; do
      python WindPredictionRNNBatchDB.py --best --early
      echo Run number $COUNTER
      let COUNTER=COUNTER+1
