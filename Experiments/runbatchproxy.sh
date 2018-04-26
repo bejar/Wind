f0b70ae4f352bb4dbeb05c761d0376a06090749b
javier.bejar <bejar@cs.upc.edu>
2018-04-26 06:18:56 +0200

    Adding number of runs to batch script

--- Experiments/runbatchproxy.sh
+++ Experiments/runbatchproxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 COUNTER=0
-while [  $COUNTER -lt 30 ]; do
+while [  $COUNTER -lt $1 ]; do
      python WindPredictionRNNBatchDB.py --best --proxy --early --gpu
      echo Run number $COUNTER
      let COUNTER=COUNTER+1
