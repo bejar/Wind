d33d565d5ce709563f37fbaf001ea931dd374b0e
javier.bejar <bejar@cs.upc.edu>
2018-06-08 10:15:28 +0200

    Fixing proxy problem

--- Wind/Util.py
+++ Wind/Util.py
@@ -18,6 +18,9 @@ Util
 """
 
 import json
+from pymongo import MongoClient
+from Wind.Private.DBConfig import mongoconnection
+import numpy as np
 
 __author__ = 'bejar'
 
@@ -44,3 +47,32 @@ def load_config_file(nfile, abspath=False, id=False):
         config['_id'] = '00000000'
 
     return config
+
+
+
+
+
+def find_exp(query):
+    client = MongoClient(mongoconnection.server)
+    db = client[mongoconnection.db]
+    db.authenticate(mongoconnection.user, password=mongoconnection.passwd)
+    col = db[mongoconnection.col]
+    return col.find(query)
+
+
+def count_exp(query):
+    client = MongoClient(mongoconnection.server)
+    db = client[mongoconnection.db]
+    db.authenticate(mongoconnection.user, password=mongoconnection.passwd)
+    col = db[mongoconnection.col]
+    print(col.count(query))
+
+
+def sel_result(lexp, ncol):
+    ldata = []
+    for exp in lexp:
+        data = np.array(exp['result'])
+        ldata.append((int(exp['data']['datanames'][0].split('-')[1]), data[:, ncol]))
+    ldata = sorted(ldata, key=lambda x: x[0])
+
+    return np.array([v[0] for v in ldata]), np.array([v[1] for v in ldata])
