dde63102056fad1016c618632dc0d477e8a997f0
javier.bejar <bejar@cs.upc.edu>
2018-07-09 07:35:50 +0200

    Updating data transformation script

--- Scripts/Generate/TransformData.py
+++ Scripts/Generate/TransformData.py
@@ -22,6 +22,7 @@ from netCDF4 import Dataset
 import numpy as np
 import time
 from Wind.Config.Paths import wind_data_path, wind_path, wind_NREL_data_path
+import argparse
 
 __author__ = 'bejar'
 
@@ -95,9 +96,17 @@ def generate_data(dfile, vars, step, mode='average'):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--sec', type=int, help='Sites Section')
+    parser.add_argument('--isite', type=int, help='Initial Site')
+    parser.add_argument('--fsite', type=int, help='Final Site')
+    parser.add_argument('--step', default=12, type=int, help='Grouping step')
+    parser.add_argument('--mode', default='average', choices=['average', 'split'], help='Grouping mode')
+    args = parser.parse_args()
 
     # Grupos de 5 minutos
-    step = 12
+    step = args.step
 
     # wfiles = ['90/45142', '90/45143',
     #           '90/45229','90/45230']
@@ -109,8 +118,8 @@ if __name__ == '__main__':
     #           '11/5793', '11/5794', '11/5795', '11/5796',
     #           '11/5752', '11/5753', '11/5754', '11/5755']
     vars = ['wind_speed', 'density', 'pressure', 'wind_direction']
-    wfiles = ['230/' + str(i) for i in range(115000, 115500) ]
+    wfiles = [str(args.sec)+'/' + str(i) for i in range(args.isite, args.fsite) ]
 
     for wf in wfiles:
         print("Processing %s" % wf)
-        generate_data(wf, vars, step, mode="average")
+        generate_data(wf, vars, step, mode=args.mode)
