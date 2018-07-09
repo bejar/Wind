0ffbade11b2db9e3ed16117b7983396483564e87
javier.bejar <bejar@cs.upc.edu>
2018-07-09 07:35:07 +0200

    Path configuration example

---
+++ Wind/Config/PathsEx.py
@@ -0,0 +1,27 @@
+"""
+.. module:: Paths
+
+Paths
+*************
+
+:Description: Paths
+
+
+
+:Authors: bejar
+
+
+:Version:
+
+:Created on: 15/06/2017 11:15
+
+"""
+
+__author__ = 'bejar'
+
+wind_path = '/Path/to/wind/directory'
+# wind_data = '/home/bejar/storage/Data/Wind/files/'
+wind_data_path = '/Path/to/Data'
+wind_data_ext = '/Path/to/external/drive/data/'
+wind_models_path = '/Path/to/stored/Models/'
+wind_NREL_data_path = '/Path/to/NREL/data'
