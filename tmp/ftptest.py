ac482623bd5f5bd7d6176da0ef737af10de39de3
javier.bejar <bejar@cs.upc.edu>
2018-07-19 10:18:19 +0200

    Remote data testing

--- tmp/ftptest.py
+++ tmp/ftptest.py
@@ -26,4 +26,5 @@ from Wind.Config import wind_data_path
 
 if __name__ == '__main__':
     srv = pysftp.Connection(host="polaris.cs.upc.edu", username='expdata')
+    print(wind_data_path + '0-0-12.py')
     srv.get(wind_data_path + '0-0-12.py', wind_data_path + '0-0-12.py')
