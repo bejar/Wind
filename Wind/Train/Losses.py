76bd900c04c50f5f54a7f5eeaeac59f6c0b7e7f5
javier.bejar <bejar@cs.upc.edu>
2018-12-05 12:21:18 +0100

    tweaking weights in loss

--- Wind/Train/Losses.py
+++ Wind/Train/Losses.py
@@ -42,6 +42,6 @@ def linear_weighted_mse(odimensions):
     """
     # weights = K.ones(odimensions)
     # l = ([1]*(odimensions-1)) + [odimensions]
-    weights = K.constant(([1]*(odimensions-1)) + [odimensions*2])
+    weights = K.constant(([1]*(odimensions-1)) + [odimensions/2])
     weights = K.reshape(weights, (1,-1, 1))
     return wrapped_partial(losses.mean_squared_error, weights=weights)
