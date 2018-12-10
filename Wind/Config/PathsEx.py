1adfaee2e85e28d9eef128491157722397d6104b
javier.bejar <bejar@cs.upc.edu>
2018-12-10 14:59:26 +0100

    Adding missing paths in PathsExp.py

--- Wind/Config/PathsEx.py
+++ Wind/Config/PathsEx.py
@@ -20,8 +20,16 @@ Paths
 __author__ = 'bejar'
 
 wind_path = '/Path/to/wind/directory'
-# wind_data = '/home/bejar/storage/Data/Wind/files/'
+
 wind_data_path = '/Path/to/Data'
 wind_data_ext = '/Path/to/external/drive/data/'
 wind_models_path = '/Path/to/stored/Models/'
 wind_NREL_data_path = '/Path/to/NREL/data'
+
+# For Minotauro Jobs
+wind_res_path = '/Path/to/Res/'
+wind_jobs_path = '/Path/to/Jobs/'
+
+# For accessing to data remotely
+remote_wind_data_path = '/home/bejar/storage/Data/Wind/Data/'
+remote_data = ('host', 'user')
