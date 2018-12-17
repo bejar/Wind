ed0b044d539f637211a28902c1bd505f56548657
javier.bejar <bejar@cs.upc.edu>
2018-12-17 09:54:31 +0100

    Fixing generating minotauro jobs, adding flag for allowing in te future to generate also jobs for Power9

--- Minotauro/ExtractConfig.py
+++ Minotauro/ExtractConfig.py
@@ -40,6 +40,8 @@ def main():
     parser.add_argument('--jph', type=int, default=30, help='Number of jobs per hour')
     parser.add_argument('--exp', default='convos2s', help='Type of configs')
     parser.add_argument('--nocopy', action='store_true', default=False, help='copy files')
+    parser.add_argument('--machine', default='mino', help='Machine the scripts are for')
+
 
     args = parser.parse_args()
 
@@ -59,8 +61,10 @@ def main():
 
     jobtime = (args.nconfig // args.jph) + 2
 
-    batchjob = open(f"'{nm}/windjob{nm}.cmd'", 'w')
-    batchjob.write("""#!/bin/bash
+    if args.machine == 'mino':
+
+        batchjob = open(f"'{nm}/windjob{nm}.cmd'", 'w')
+        batchjob.write("""#!/bin/bash
 # @ job_name = windjob
 # @ initialdir = /gpfs/projects/bsc28/bsc28642/Wind/Code/Wind/Experiments
 # @ output = windjob%j.out
@@ -70,30 +74,34 @@ def main():
 # @ cpus_per_task = 1
 # @ features = k80
 """ +
-                   f"# @ wall_clock_limit = {jobtime}:30:00\n"
-                   +
-                   """module purge
-                   module purge; module load K80 impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
-                   PYTHONPATH=/gpfs/projects/bsc28/bsc28642/Wind/Code/Wind/
-                   export PYTHONPATH
-                   
-                   """)
+                       f"# @ wall_clock_limit = {jobtime}:30:00\n"
+                       +
+                       """module purge
+                       module load K80 cuda/8.0 mkl/2017.1 CUDNN/5.1.10-cuda_8.0 intel-opencl/2016 python/3.6.0+_ML
+                       PYTHONPATH=/gpfs/projects/bsc28/bsc28642/Wind/Code/Wind/
+                       export PYTHONPATH
+                       
+                       """)
 
-    if len(lconfig) > 0:
-        for config in lconfig:
-            if not args.nocopy:
-                print(config['_id'])
-                sconf = json.dumps(config)
-                fconf = open(f"./{nm}/Jobs/{config['_id']}.json", 'w')
-                fconf.write(sconf + '\n')
-                fconf.close()
-                copy(f"{wind_data_path}/{config['data']['datanames'][0]}.npy", f"./{nm}/Data/")
-            batchjob.write(
-                f"python WindExperimentBatch.py --best --early --gpu --mino --config {config['_id']}\n")
-            col.update({'_id': config['_id']}, {'$set': {'status': 'extract'}})
-    batchjob.close()
-    print(f"NCONF= {len(lconfig)}")
+        if len(lconfig) > 0:
+            for config in lconfig:
+                if not args.nocopy:
+                    print(config['_id'])
+                    sconf = json.dumps(config)
+                    fconf = open(f"./{nm}/Jobs/{config['_id']}.json", 'w')
+                    fconf.write(sconf + '\n')
+                    fconf.close()
+                    copy(f"{wind_data_path}/{config['data']['datanames'][0]}.npy", f"./{nm}/Data/")
+                batchjob.write(
+                    f"python WindExperimentBatch.py --best --early --gpu --mino --config {config['_id']}\n")
+                col.update({'_id': config['_id']}, {'$set': {'status': 'extract'}})
+        batchjob.close()
 
+
+        print(f"NCONF= {len(lconfig)}")
+
+
+# module load K80 impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
 
 if __name__ == '__main__':
     main()
