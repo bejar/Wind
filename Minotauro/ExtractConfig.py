fd8a73b58f8f54fe1eeddd02f274de89a19759c2
javier.bejar <bejar@cs.upc.edu>
2019-02-04 13:46:10 +0100

    Deleting old reservation configuration for power9 upgrade

--- Minotauro/ExtractConfig.py
+++ Minotauro/ExtractConfig.py
@@ -115,7 +115,6 @@ export PYTHONPATH
 #SBATCH --cpus-per-task=1
 #SBATCH --time={jobtime}:50:00
 #SBATCH --gres=gpu:1
-#SBATCH --reservation=test-rhel-7.5
 module purge
 module load  gcc/6.4.0  cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 opencv/3.4.1 python/3.6.5_ML
 PYTHONPATH={jobs_code_path}
