e57cd6ceb5020b3a8c4a5478bc09faa49a44aac8
javier.bejar <bejar@cs.upc.edu>
2020-08-13 15:25:35 +0200

    Tweaking measures launch script

--- Scripts/Measures/DataMeasures.py
+++ Scripts/Measures/DataMeasures.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     windows = {'12h':12, '24h':24, '1w':168, '1m':720, '3m':2190, '6m':4380}
     if (args.isite+args.nsites) <= lim_sites:
-        lsites = range(args.isite, args.nsites+args.nsites)
+        lsites = range(args.isite, args.isites+args.nsites)
     else:
         lsites = range(args.isite, lim_sites)
     ncores = args.ncores  # multiprocessing.cpu_count()
