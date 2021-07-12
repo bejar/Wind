d585aa087dc8893fb3259ab034d9f6d1eff2fdca
Javier Bejar <bejar@cs.upc.edu>
2021-07-12 10:08:43 +0200

    Update for experiments

--- Wind/Data/DataSet.py
+++ Wind/Data/DataSet.py
@@ -775,17 +775,13 @@ class Dataset:
 
 
         # Remove all sites that have a correlation outside a limit
-        dr = []
         if 'corr' in self.config:
             cmin,cmax = self.config['corr']
             for d in wind:
                 if d != datanames[0]:
                     if not (cmin < np.corrcoef(wind[d][:,0], wind[datanames[0]][:,0])[0,1]< cmax):
-                        dr.append(d)
-        for d in dr:
-            del wind[d]
-
-        for d in wind:
+                        datanames.remove(d)
+        for d in datanames:
             print(d)
 
         if (self.config['dataset'] == 0) or (self.config['dataset'] == 'onesiteonevar'):
