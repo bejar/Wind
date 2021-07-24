4151b8f6c274586e11ee8af4c420f9d23fbd717f
javier.bejar <bejar@cs.upc.edu>
2021-07-24 07:16:42 +0200

    Testing separate branch for close sites

--- Scripts/Dispatch/Dispatcher.py
+++ Scripts/Dispatch/Dispatcher.py
@@ -125,6 +125,7 @@ if __name__ == '__main__':
                 os.rename(file, f'{file.replace(".json", ".done")}')
 
         for d in done:
+            print(f'{file.replace(".json", ".done")}')
             os.remove(f'{file.replace(".json", ".done")}')
 
         print(f'it {n}  uploaded = {len(lres)} ---------------------------------------------------------')
